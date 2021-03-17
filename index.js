2dd59bff6d93bd980e34c10176daab856a813305
LoraZahorzhevska <Zagorzhevskaya@gmail.com>
2021-03-17 23:48:34 +0200

    финиш

--- index.js
+++ index.js
@@ -1,59 +1,79 @@
 import galleryItems from "./gallery-items.js";
 
 const galleryContainer = document.querySelector(".js-gallery");
-const lightboxImg = document.querySelector(".lightbox__image");
-const divLigthbox = document.querySelector('.js-lightbox');
-const lightboxImage = document.querySelector('.ightbox__image');
-const btnClose = document.querySelector('.lightbox__button');
-const divOverlay = document.querySelector('.lightbox__overlay');
-const cardsMarkup = createCardsGallary(galleryItems);
+const divLigthbox = document.querySelector(".js-lightbox");
+const lightboxImage = document.querySelector(".lightbox__image");
+const btnClose = document.querySelector(".lightbox__button");
+const divOverlay = document.querySelector(".lightbox__overlay");
 
 // создание разметки
 
-function createCardsGallary(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `
+galleryItems.map((el, index) => {
+  galleryContainer.innerHTML += `
 <li class="gallery__item">
     <a
     class="gallery__link"
-    href='${original}'
+    href='${el.original}'
   >
   <img
   class="gallery__image"
-  src="${preview}"
-  data-source="${original}"
-  alt="${description}"
+  src="${el.preview}"
+  data-source="${el.original}"
+  alt="${el.description}"
+  data-index="${index}"
   />
   </a>
   </li>`;
-    })
-    .join(" ");
-}
+});
 
-galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup); //добавление разметки в HTML
-
-
-galleryItems.addEventListener('click' , (evt) => {
+galleryContainer.addEventListener("click", (evt) => {
   evt.preventDefault(); // отменяет действие браузера по умолчанию
   let modalLink = evt.target.dataset.source;
-  divLigthbox.classList.add('is-open');
+  divLigthbox.classList.add("is-open");
   lightboxImage.src = modalLink;
   lightboxImage.dataset.index = evt.target.dataset.index;
 });
 
+btnClose.addEventListener("click", closeOverlay);
+divOverlay.addEventListener("click", closeOverlay);
 
-btnClose.addEventListener('click' , closeOverlay);
-divOverlay.addEventListener('click' , closeOverlay);
-
-window.addEventListener('keydown' , (evt) => {
-  if (evt.key === 'Escape') {
-    closeOverlay()
+window.addEventListener("keydown", (evt) => {
+  if (evt.key === "Escape") {
+    closeOverlay();
   }
-  if (evt.key === 'ArrowLeft') {
-    arrowLeft()
+  if (evt.key === "ArrowLeft") {
+    arrowLeft();
   }
-  if (evt.key === 'ArrowRight') {
-    arrowRight()
+  if (evt.key === "ArrowRight") {
+    arrowRight();
   }
 });
+
+function closeOverlay() {
+  divLigthbox.classList.remove("is-open");
+  lightboxImage.src = "";
+}
+
+function newSrc(step, index) {
+  lightboxImage.dataset.index = `${index + step}`;
+  lightboxImage.src = galleryItems[index + step].original;
+}
+
+function arrowLeft() {
+  let index = Number(lightboxImage.dataset.index);
+  if (index === 0) {
+    newSrc(0, galleryItems.length - 1);
+    return;
+  }
+  //console.log(index);
+  newSrc(-1, index);
+}
+
+function arrowRight() {
+  let index = Number(lightboxImage.dataset.index);
+  if (index === galleryItems.length - 1) {
+    newSrc(0, 0);
+  }
+  //console.log(index);
+  newSrc(1, index);
+}
